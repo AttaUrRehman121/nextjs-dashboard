cc9305d32dd5eaa66879c1af2886f2e1f6aa3469
ATTA UR REHMAN <manirana770@gmail.com>
2025-07-23 11:43:41 +0500

    sAS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
 import '@/app/ui/global.css';
+import { RobotoFont } from './ui/fonts/font';
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" >
+      <body className={`${RobotoFont.className}`}>{children}</body>
     </html>
   );
 }
