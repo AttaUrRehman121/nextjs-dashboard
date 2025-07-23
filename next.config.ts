084e744a4b5ad9bb055275b2db0cad78e2fefeb1
ATTA UR REHMAN <manirana770@gmail.com>
2025-07-23 14:22:40 +0500

    dfg

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    ppr: 'incremental' // Enable Partial Prerendering its help to improve performance 
-  }
+  /* config options here */
 };
 
 export default nextConfig;
